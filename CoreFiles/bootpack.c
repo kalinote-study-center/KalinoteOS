8af0fa765479585f97a8eff6e223948aed548112
killynote <840746219@qq.com>
2020-10-06 00:52:15 +0800

    update naskfunc and bootpack

--- CoreFiles/bootpack.c
+++ CoreFiles/bootpack.c
@@ -1,22 +1,74 @@
 //naskfunc中的函数
 void io_hlt(void);		//hlt功能
-void write_mem8(int addr, int data);		//写入内存
+//void write_mem8(int addr, int data);		//写入内存
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
 
+//此文件中的函数
+void KaliMain(void);		//主函数
+void init_palette(void);	//初始化调色板函数
+void set_palette(int start, int end, unsigned char *rgb);		//设置调色板
 
 void KaliMain(void){
 	/*这里是主程序*/
 	int i;
 	char *p;
 	
-	for(i = 0xa0000; i <= 0xaffff; i++){
-		p = (char *) i;
-		*p = i & 0x0f;
+	init_palette();
+	
+	p = (char *) 0xa0000;	//指定地址
+	
+	for (i = 0; i <= 0xffff; i++) {
+		p[i] = i & 0x0f;
 	}
 	
 	for(;;){
 		//停止CPU
 		io_hlt();
 	}
+}
+
+void init_palette(void){
+	/*调色板函数，预置15中基本颜色，可以自行添加*/
+	static unsigned char table_rgb[16 * 3] = {
+		0x00, 0x00, 0x00,	/*  0:黑 */
+		0xff, 0x00, 0x00,	/*  1:亮红 */
+		0x00, 0xff, 0x00,	/*  2:亮绿 */
+		0xff, 0xff, 0x00,	/*  3:亮黄 */
+		0x00, 0x00, 0xff,	/*  4:亮蓝 */
+		0xff, 0x00, 0xff,	/*  5:亮紫 */
+		0x00, 0xff, 0xff,	/*  6:浅亮蓝 */
+		0xff, 0xff, 0xff,	/*  7:白 */
+		0xc6, 0xc6, 0xc6,	/*  8:亮灰 */
+		0x84, 0x00, 0x00,	/*  9:暗红 */
+		0x00, 0x84, 0x00,	/* 10:暗绿 */
+		0x84, 0x84, 0x00,	/* 11:暗黄 */
+		0x00, 0x00, 0x84,	/* 12:暗青 */
+		0x84, 0x00, 0x84,	/* 13:暗紫 */
+		0x00, 0x84, 0x84,	/* 14:浅暗蓝 */
+		0x84, 0x84, 0x84	/* 15:暗灰 */
+	};
+	set_palette(0, 15, table_rgb);
+	return;
+	// C语言中的static char只能用于数据，相当于汇编中的DB
+}
+
+void set_palette(int start, int end, unsigned char *rgb){
+	int i, eflags;
+	eflags = io_load_eflags();	/* 记录中断许可标志的值 */
+	io_cli(); 					/* 将中断许可标志置为0，禁止中断 */
+	io_out8(0x03c8, start);
+	for (i = start; i <= end; i++) {
+		io_out8(0x03c9, rgb[0] / 4);
+		io_out8(0x03c9, rgb[1] / 4);
+		io_out8(0x03c9, rgb[2] / 4);
+		rgb += 3;
+	}
+	io_store_eflags(eflags);	/* 复原中断许可标志 */
+	return;
 }
 
 void HariMain(void){
