c7dc75531e40514c3b966f40dd25c489ef1670c6
killynote <840746219@qq.com>
2020-10-06 23:00:17 +0800

    Optimize code structure

--- CoreFiles/bootpack.c
+++ CoreFiles/bootpack.c
@@ -2,16 +2,22 @@
 #include <stdio.h>
 #include "bootpack.h"
 
+struct MOUSE_DEC {
+	unsigned char buf[3], phase;
+};
+
 extern struct FIFO8 keyfifo, mousefifo;
-void enable_mouse(void);			//激活鼠标
-void init_keyboard(void);			//初始化键盘控制电路
+void enable_mouse(struct MOUSE_DEC *mdec);							//激活鼠标
+void init_keyboard(void);											//初始化键盘控制电路
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat);
+extern struct FIFO8 keyfifo, mousefifo;			
 
 void KaliMain(void){
 	/*这里是主程序*/
 	struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;		//启动信息(BOOTINFO结构体)
 	char s[40], mcursor[256], keybuf[32], mousebuf[128];
 	int mx, my, i;
-	unsigned char mouse_dbuf[3], mouse_phase;
+	struct MOUSE_DEC mdec;
 	
 	init_gdtidt();
 	init_pic();
@@ -22,7 +28,7 @@ void KaliMain(void){
 	io_out8(PIC1_IMR, 0xef); /* 允许鼠标(11101111) */
 	
 	init_keyboard();		//初始化键盘
-	
+
 	init_palette();												//初始化调色板
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);		//初始化屏幕
 	/*
@@ -52,8 +58,7 @@ void KaliMain(void){
 	//sprintf(s, "scrnx = %d", binfo->scrnx);
 	//putfonts8_asc(binfo->vram, binfo->scrnx, 16, 64, COL_WHITE, s);
 	
-	enable_mouse();			//激活鼠标
-	mouse_phase = 0;		//进入到等待鼠标的0xfa状态
+	enable_mouse(&mdec);			//激活鼠标
 	
 	for(;;){
 		//停止CPU
@@ -70,25 +75,9 @@ void KaliMain(void){
 			} else if (fifo8_status(&mousefifo) != 0) {
 				i = fifo8_get(&mousefifo);
 				io_sti();
-				if (mouse_phase == 0) {
-					/* 等待鼠标的0xfa状态 */
-					if (i == 0xfa) {
-						mouse_phase = 1;
-					}
-				} else if (mouse_phase == 1) {
-					/* 等待鼠标的第一字节 */
-					mouse_dbuf[0] = i;
-					mouse_phase = 2;
-				} else if (mouse_phase == 2) {
-					/* 等待鼠标的第二字节 */
-					mouse_dbuf[1] = i;
-					mouse_phase = 3;
-				} else if (mouse_phase == 3) {
-					/* 等待鼠标的第三字节 */
-					mouse_dbuf[2] = i;
-					mouse_phase = 1;
-					/* 三个字节收集完毕，显示出来 */
-					sprintf(s, "%02X %02X %02X", mouse_dbuf[0], mouse_dbuf[1], mouse_dbuf[2]);
+				if (mouse_decode(&mdec, i) != 0) {
+					/* 3字节集齐，显示出来 */
+					sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
 					boxfill8(binfo->vram, binfo->scrnx, COL_LDBLUE, 32, 16, 32 + 8 * 8 - 1, 31);
 					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL_WHITE, s);
 				}
@@ -105,8 +94,7 @@ void KaliMain(void){
 #define KEYCMD_WRITE_MODE		0x60
 #define KBC_MODE				0x47
 
-void wait_KBC_sendready(void)
-{
+void wait_KBC_sendready(void){
 	/* 等待键盘控制电路准备完毕 - 此处原内容在第140页 */
 	for (;;) {
 		if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY) == 0) {
@@ -116,8 +104,7 @@ void wait_KBC_sendready(void)
 	return;
 }
 
-void init_keyboard(void)
-{
+void init_keyboard(void){
 	/* 初始化键盘控制电路 */
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
@@ -129,16 +116,45 @@ void init_keyboard(void)
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
 
-void enable_mouse(void)
-{
+void enable_mouse(struct MOUSE_DEC *mdec){
 	/* 激活鼠标 */
 	wait_KBC_sendready();
 	io_out8(PORT_KEYCMD, KEYCMD_SENDTO_MOUSE);
 	wait_KBC_sendready();
 	io_out8(PORT_KEYDAT, MOUSECMD_ENABLE);
-	return; /* うまくいくとACK(0xfa)が送信されてくる */
+	/* 没有问题的话，ACK(0xfa)会被传过来 */
+	mdec->phase = 0; /* 等待0xfa的阶段 */
+	return;
 }
 
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
+	if (mdec->phase == 0) {
+		/* 等待0xfa的阶段 */
+		if (dat == 0xfa) {
+			mdec->phase = 1;
+		}
+		return 0;
+	}
+	if (mdec->phase == 1) {
+		/* 等待鼠标第一字节的阶段 */
+		mdec->buf[0] = dat;
+		mdec->phase = 2;
+		return 0;
+	}
+	if (mdec->phase == 2) {
+		/* 等待鼠标第二字节的阶段 */
+		mdec->buf[1] = dat;
+		mdec->phase = 3;
+		return 0;
+	}
+	if (mdec->phase == 3) {
+		/* 等待鼠标第三字节的阶段 */
+		mdec->buf[2] = dat;
+		mdec->phase = 1;
+		return 1;
+	}
+	return -1; /* 出错 */
+}
 
 void HariMain(void){
 	/*系统启动入口*/
