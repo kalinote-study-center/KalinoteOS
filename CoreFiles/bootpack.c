6ff3c0f787560c8e131cc742ceef37f63e4e3da7
killynote <840746219@qq.com>
2020-10-06 15:49:18 +0800

    add gdt and idt

--- CoreFiles/bootpack.c
+++ CoreFiles/bootpack.c
@@ -1,25 +1,5 @@
 #include <stdio.h>
 
-//naskfunc中的函数(汇编编写)
-void io_hlt(void);							//hlt功能
-//void write_mem8(int addr, int data);		//写入内存(被指针取代)
-void io_cli(void);							//禁止中断
-void io_out8(int port, int data);			//传输数据用的
-int io_load_eflags(void);					//读取最初的eflags值
-void io_store_eflags(int eflags);			//将值存入eflags寄存器
-
-//此文件中的函数
-void KaliMain(void);																				//主函数
-void init_palette(void);																			//初始化调色板函数
-void set_palette(int start, int end, unsigned char *rgb);											//设置调色板
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);		//绘制方块
-void init_screen(char *vram, int x, int y);															//初始化屏幕
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);								//绘制字体
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);					//绘制字符串
-void init_mouse_cursor8(char *mouse, char bc);														//初始化鼠标指针
-void putblock8_8(char *vram, int vxsize, int pxsize,
-	int pysize, int px0, int py0, char *buf, int bxsize);											//鼠标背景色处理
-
 // 15种颜色常数定义
 #define COL_BLACK		0
 #define COL_BRED		1
@@ -38,6 +18,7 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 #define COL_LDBLUE		14
 #define COL_DGREY		15
 
+//结构体
 struct BOOTINFO {
 	/*启动信息 - 此处原内容在第89页*/
 	char cyls, leds, vmode, reserve;
@@ -45,11 +26,52 @@ struct BOOTINFO {
 	char *vram;
 };
 
+struct SEGMENT_DESCRIPTOR {
+	/*存放GDT的8字节内容 - 此处原内容在第105页*/
+	short limit_low, base_low;
+	char base_mid, access_right;
+	char limit_high, base_high;
+};
+
+struct GATE_DESCRIPTOR {
+	/*存放IDT的8字节内容 - 此处原内容在第105页*/
+	short offset_low, selector;
+	char dw_count, access_right;
+	short offset_high;
+};
+
+//naskfunc中的函数(汇编编写)
+void io_hlt(void);							//hlt功能
+//void write_mem8(int addr, int data);		//写入内存(被指针取代)
+void io_cli(void);							//禁止中断
+void io_out8(int port, int data);			//传输数据用的
+int io_load_eflags(void);					//读取最初的eflags值
+void io_store_eflags(int eflags);			//将值存入eflags寄存器
+void load_gdtr(int limit, int addr);		//加载GDTR寄存器
+void load_idtr(int limit, int addr);		//加载IDTR寄存器
+
+//此文件中的函数
+void KaliMain(void);																				//主函数
+void init_palette(void);																			//初始化调色板函数
+void set_palette(int start, int end, unsigned char *rgb);											//设置调色板
+void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);		//绘制方块
+void init_screen(char *vram, int x, int y);															//初始化屏幕
+void putfont8(char *vram, int xsize, int x, int y, char c, char *font);								//绘制字体
+void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);					//绘制字符串
+void init_mouse_cursor8(char *mouse, char bc);														//初始化鼠标指针
+void putblock8_8(char *vram, int vxsize, int pxsize,
+	int pysize, int px0, int py0, char *buf, int bxsize);											//鼠标背景色处理
+void init_gdtidt(void);																				//初始化GDT和IDT
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
+
 void KaliMain(void){
 	/*这里是主程序*/
 	struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;		//启动信息(BOOTINFO结构体)
 	char s[40], mcursor[256];
+	int mx, my;
 	
+	init_gdtidt();
 	init_palette();												//初始化调色板
 	init_screen(binfo->vram, binfo->scrnx, binfo->scrny);		//初始化屏幕
 	/*
@@ -57,11 +79,13 @@ void KaliMain(void){
 	* 其他的同理
 	*/
 	
+	mx = (binfo->scrnx - 16) / 2; 								//鼠标指针位置，默认为屏幕中心
+	my = (binfo->scrny - 28 - 16) / 2;
 	init_mouse_cursor8(mcursor, COL_LDBLUE);					//初始化鼠标指针
 	putblock8_8(binfo->vram, binfo->scrnx, 16, 16,
-		(binfo->scrnx - 16) / 2,
-		(binfo->scrny - 28 - 16) / 2,
-		mcursor, 16);											//绘制鼠标
+		mx, my, mcursor, 16);									//绘制鼠标
+	sprintf(s, "(%d, %d)", mx, my);
+	putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL_WHITE, s);
 	/*
 	* 注：此时的鼠标背景只能是COL_LDBLUE的纯色，所以移动是会覆盖掉下面的内容
 	* 不过后面会解决这个问题
@@ -220,6 +244,58 @@ void putblock8_8(char *vram, int vxsize, int pxsize,
 	}
 	return;
 }
+
+
+void init_gdtidt(void)
+{
+	/*初始化GDT和IDT - 此处原内容在第页105*/
+	struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;		//GDT的地址位于0x270000~0x27ffff
+	struct GATE_DESCRIPTOR    *idt = (struct GATE_DESCRIPTOR    *) 0x0026f800;		//IDT的地址位于0x26f800~0x26ffff
+	int i;
+
+	/* GDT初始化 */
+	for (i = 0; i < 8192; i++) {
+		set_segmdesc(gdt + i, 0, 0, 0);
+	}
+	set_segmdesc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+	set_segmdesc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+	load_gdtr(0xffff, 0x00270000);
+
+	/* IDT初始化 */
+	for (i = 0; i < 256; i++) {
+		set_gatedesc(idt + i, 0, 0, 0);
+	}
+	load_idtr(0x7ff, 0x0026f800);
+
+	return;
+}
+
+void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar)
+{
+	
+	if (limit > 0xfffff) {
+		ar |= 0x8000; /* G_bit = 1 */
+		limit /= 0x1000;
+	}
+	sd->limit_low    = limit & 0xffff;
+	sd->base_low     = base & 0xffff;
+	sd->base_mid     = (base >> 16) & 0xff;
+	sd->access_right = ar & 0xff;
+	sd->limit_high   = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+	sd->base_high    = (base >> 24) & 0xff;
+	return;
+}
+
+void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar)
+{
+	gd->offset_low   = offset & 0xffff;
+	gd->selector     = selector;
+	gd->dw_count     = (ar >> 8) & 0xff;
+	gd->access_right = ar & 0xff;
+	gd->offset_high  = (offset >> 16) & 0xffff;
+	return;
+}
+
 
 void HariMain(void){
 	/*系统启动入口*/
