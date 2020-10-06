383c61d1f7d84afa8e665f93057bfbc499ee1cb7
killynote <840746219@qq.com>
2020-10-06 23:19:57 +0800

    Optimize mouse data transmission

--- CoreFiles/bootpack.c
+++ CoreFiles/bootpack.c
@@ -4,6 +4,7 @@
 
 struct MOUSE_DEC {
 	unsigned char buf[3], phase;
+	int x, y, btn;
 };
 
 extern struct FIFO8 keyfifo, mousefifo;
@@ -77,12 +78,22 @@ void KaliMain(void){
 				io_sti();
 				if (mouse_decode(&mdec, i) != 0) {
 					/* 3字节集齐，显示出来 */
-					sprintf(s, "%02X %02X %02X", mdec.buf[0], mdec.buf[1], mdec.buf[2]);
-					boxfill8(binfo->vram, binfo->scrnx, COL_LDBLUE, 32, 16, 32 + 8 * 8 - 1, 31);
+					sprintf(s, "[lcr %4d %4d]", mdec.x, mdec.y);
+					if ((mdec.btn & 0x01) != 0) {
+						s[1] = 'L';
+					}
+					if ((mdec.btn & 0x02) != 0) {
+						s[3] = 'R';
+					}
+					if ((mdec.btn & 0x04) != 0) {
+						s[2] = 'C';
+					}
+					boxfill8(binfo->vram, binfo->scrnx, COL_LDBLUE, 32, 16, 32 + 15 * 8 - 1, 31);
 					putfonts8_asc(binfo->vram, binfo->scrnx, 32, 16, COL_WHITE, s);
 				}
 			}
 		}
+		
 	}
 }
 
@@ -127,7 +138,8 @@ void enable_mouse(struct MOUSE_DEC *mdec){
 	return;
 }
 
-int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
+int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat)
+{
 	if (mdec->phase == 0) {
 		/* 等待0xfa的阶段 */
 		if (dat == 0xfa) {
@@ -137,8 +149,11 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
 	}
 	if (mdec->phase == 1) {
 		/* 等待鼠标第一字节的阶段 */
-		mdec->buf[0] = dat;
-		mdec->phase = 2;
+		if ((dat & 0xc8) == 0x08) {
+			/* 如果第一字节正确 */
+			mdec->buf[0] = dat;
+			mdec->phase = 2;
+		}
 		return 0;
 	}
 	if (mdec->phase == 2) {
@@ -151,10 +166,21 @@ int mouse_decode(struct MOUSE_DEC *mdec, unsigned char dat){
 		/* 等待鼠标第三字节的阶段 */
 		mdec->buf[2] = dat;
 		mdec->phase = 1;
+		mdec->btn = mdec->buf[0] & 0x07;
+		mdec->x = mdec->buf[1];
+		mdec->y = mdec->buf[2];
+		if ((mdec->buf[0] & 0x10) != 0) {
+			mdec->x |= 0xffffff00;
+		}
+		if ((mdec->buf[0] & 0x20) != 0) {
+			mdec->y |= 0xffffff00;
+		}
+		mdec->y = - mdec->y; /* 鼠标的y方向与画面符号相反 */
 		return 1;
 	}
 	return -1; /* 出错 */
 }
+
 
 void HariMain(void){
 	/*系统启动入口*/
