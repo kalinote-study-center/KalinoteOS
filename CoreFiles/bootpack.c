712bf2063d5d449274414c0d2c0964ec5730ee0d
killynote <840746219@qq.com>
2020-10-07 22:55:59 +0800

    Modify display logic

--- CoreFiles/bootpack.c
+++ CoreFiles/bootpack.c
@@ -109,17 +109,18 @@ void KaliMain(void){
 					/* 鼠标指针移动 */
 					mx += mdec.x;
 					my += mdec.y;
+					//限制鼠标指针边界
 					if (mx < 0) {
 						mx = 0;
 					}
 					if (my < 0) {
 						my = 0;
 					}
-					if (mx > binfo->scrnx - 16) {
-						mx = binfo->scrnx - 16;
+					if (mx > binfo->scrnx - 1) {
+						mx = binfo->scrnx - 1;
 					}
-					if (my > binfo->scrny - 16) {
-						my = binfo->scrny - 16;
+					if (my > binfo->scrny - 1) {
+						my = binfo->scrny - 1;
 					}
 					sprintf(s, "(%3d, %3d)", mx, my);
 					boxfill8(buf_back, binfo->scrnx, COL_LDBLUE, 0, 0, 79, 15); /* 隐藏坐标 */
