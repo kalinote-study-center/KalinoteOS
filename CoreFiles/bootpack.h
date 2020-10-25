f2b8ecb69b96a671887c29d7cf069a89e9d3e507
killynote <840746219@qq.com>
2020-10-25 14:38:38 +0800

    The program is optimized

--- CoreFiles/bootpack.h
+++ CoreFiles/bootpack.h
@@ -33,8 +33,8 @@ void asm_inthandler2c(void);				//2c号中断，注册在0x2c
 unsigned int memtest_sub(
 	unsigned int start,
 	unsigned int end);						//读取内存
-void farjmp(int eip, int cs);				//切换任务
-
+void farjmp(int eip, int cs);				//指令跳转
+void farcall(int eip, int cs);				//转移到调用的子程序(指定偏移)
 //graphic.c(画面显示)
 void init_palette(void);																			//初始化调色板函数
 void set_palette(int start, int end, unsigned char *rgb);											//设置调色板
--- CoreFiles/hlt.nas
+++ CoreFiles/hlt.nas
@@ -1,7 +1,12 @@
 [BITS 32]
-		MOV		AL,'A'
-		CALL    2*8:0xc1a
-fin:
-		HLT
-		JMP		fin
-	
+		MOV		AL,'h'
+		CALL    2*8:0xc1f
+		MOV		AL,'e'
+		CALL    2*8:0xc1f
+		MOV		AL,'l'
+		CALL    2*8:0xc1f
+		MOV		AL,'l'
+		CALL    2*8:0xc1f
+		MOV		AL,'o'
+		CALL    2*8:0xc1f
+		RETF
