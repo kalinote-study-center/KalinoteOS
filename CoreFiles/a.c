0b2bd83d556a4a744c3000ab3f2d85b20047bee8
killynote <840746219@qq.com>
2020-10-25 17:11:54 +0800

    Writing external programs by C

---
+++ CoreFiles/a.c
@@ -0,0 +1,9 @@
+/* 简单的应用程序 */
+
+void api_putchar(int c);
+
+void KaliMain(void)
+{
+	api_putchar('A');
+	return;
+}
---
+++ CoreFiles/hello3.c
@@ -0,0 +1,13 @@
+/* 简单的应用程序 */
+
+void api_putchar(int c);
+
+void KaliMain(void)
+{
+	api_putchar('h');
+	api_putchar('e');
+	api_putchar('l');
+	api_putchar('l');
+	api_putchar('o');
+	return;
+}
