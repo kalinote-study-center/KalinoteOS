ccba3090149cd031345ad024f1c10be2ccd804e2
killynote <840746219@qq.com>
2020-10-28 11:38:46 +0800

    Optimizes the processing logic when forcing an application to end

--- CoreFiles/console.c
+++ CoreFiles/console.c
@@ -255,6 +255,8 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline){
 	char name[18], *p, *q;
 	struct TASK *task = task_now();
 	int i, segsiz, datsiz, esp, dathrb;
+	struct SHTCTL *shtctl;
+	struct SHEET *sht;
 
 	/* 根据命令生成文件名 */
 	for (i = 0; i < 13; i++) {
@@ -294,6 +296,14 @@ int cmd_app(struct CONSOLE *cons, int *fat, char *cmdline){
 				q[esp + i] = p[dathrb + i];
 			}
 			start_app(0x1b, 1003 * 8, esp, 1004 * 8, &(task->tss.esp0));
+			shtctl = (struct SHTCTL *) *((int *) 0x0fe4);
+			for (i = 0; i < MAX_SHEETS; i++) {
+				sht = &(shtctl->sheets0[i]);
+				if (sht->flags != 0 && sht->task == task) {
+					/* 找到被应用程序遗留的窗口 */
+					sheet_free(sht);	/* 关闭 */
+				}
+			}
 			memman_free_4k(memman, (int) q, segsiz);
 		} else {
 			cons_putstr0(cons, "\nUnrecognized file format.\n");
@@ -334,6 +344,7 @@ int *kal_api(int edi, int esi, int ebp, int esp, int ebx, int edx, int ecx, int 
 	} else if (edx == 5) {
 		//生成窗口
 		sht = sheet_alloc(shtctl);
+		sht->task = task;
 		sheet_setbuf(sht, (char *) ebx + ds_base, esi, edi, eax);
 		make_window8((char *) ebx + ds_base, esi, edi, (char *) ecx + ds_base, 0);
 		sheet_slide(sht, 100, 50);
