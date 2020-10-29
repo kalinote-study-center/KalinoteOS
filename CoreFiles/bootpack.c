aeff285f86ba57919433cb4f5392ea4ac095ec19
killynote <840746219@qq.com>
2020-10-29 13:34:14 +0800

    remove no use window

--- CoreFiles/bootpack.c
+++ CoreFiles/bootpack.c
@@ -4,8 +4,8 @@
 
 #define KEYCMD_LED		0xed
 
-int keywin_off(struct SHEET *key_win, struct SHEET *sht_win, int cur_c, int cur_x);			//控制窗口标题栏颜色和光标激活状态
-int keywin_on(struct SHEET *key_win, struct SHEET *sht_win, int cur_c);						//控制窗口标题栏颜色和光标激活状态
+void keywin_off(struct SHEET *key_win);														//控制窗口标题栏颜色和光标激活状态
+void keywin_on(struct SHEET *key_win);														//控制窗口标题栏颜色和光标激活状态
 
 void KaliMain(void){
 	/* 系统入口 */
@@ -15,21 +15,20 @@ void KaliMain(void){
 	struct SHTCTL *shtctl;
 	char s[40];
 	struct FIFO32 fifo, keycmd;
-	int fifobuf[128], keycmd_buf[32];
-	int mx, my, i, cursor_x, cursor_c;
+	int fifobuf[128], keycmd_buf[32], *cons_fifo[2];
+	int mx, my, i;
 	unsigned int memtotal;
 	struct MOUSE_DEC mdec;
 	struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
-	unsigned char *buf_back, buf_mouse[256], *buf_win, *buf_cons[2];
-	struct SHEET *sht_back, *sht_mouse, *sht_win, *sht_cons[2];
+	unsigned char *buf_back, buf_mouse[256], *buf_cons[2];
+	struct SHEET *sht_back, *sht_mouse, *sht_cons[2];
 	struct TASK *task_a, *task_cons[2], *task;
-	struct TIMER *timer;
 	int key_shift = 0, key_leds = (binfo->leds >> 4) & 7, keycmd_wait = -1;
 	int j, x, y, mmx = -1, mmy = -1;
 	struct SHEET *sht = 0, *key_win;
-	static char keytable0[0x80] = {									//字符编码表(后面需要按照中文键盘优化符号)
-		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
-		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
+	static char keytable0[0x80] = {
+		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0x08, 0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0x0a, 0, 'A', 'S',
 		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
 		'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
 		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
@@ -37,9 +36,9 @@ void KaliMain(void){
 		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
 		0,   0,   0,   0x5c, 0,  0,   0,   0,   0,   0,   0,   0,   0,   0x5c, 0,  0
 	};
-	static char keytable1[0x80] = {									// shift
-		0,   0,   '!', 0x22, '#', '$', '%', '&', 0x27, '(', ')', '~', '=', '~', 0,   0,
-		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '`', '{', 0,   0,   'A', 'S',
+	static char keytable1[0x80] = {
+		0,   0,   '!', 0x22, '#', '$', '%', '&', 0x27, '(', ')', '~', '=', '~', 0x08, 0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '`', '{', 0x0a, 0, 'A', 'S',
 		'D', 'F', 'G', 'H', 'J', 'K', 'L', '+', '*', 0,   0,   '}', 'Z', 'X', 'C', 'V',
 		'B', 'N', 'M', '<', '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
 		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
@@ -98,19 +97,9 @@ void KaliMain(void){
 		task_run(task_cons[i], 2, 2); /* level=2, priority=2 */
 		sht_cons[i]->task = task_cons[i];
 		sht_cons[i]->flags |= 0x20;	/* 有光标 */
+		cons_fifo[i] = (int *) memman_alloc_4k(memman, 128 * 4);
+		fifo32_init(&task_cons[i]->fifo, 128, cons_fifo[i], task_cons[i]);
 	}
-
-	/* sht_win */
-	sht_win   = sheet_alloc(shtctl);
-	buf_win   = (unsigned char *) memman_alloc_4k(memman, 160 * 52);
-	sheet_setbuf(sht_win, buf_win, 144, 52, -1); /* 无透明色 */
-	make_window8(buf_win, 144, 52, "task_a", 1);
-	make_textbox8(sht_win, 8, 28, 128, 16, COL_WHITE);
-	cursor_x = 8;
-	cursor_c = COL_WHITE;
-	timer = timer_alloc();
-	timer_init(timer, &fifo, 1);
-	timer_settime(timer, 50);
 
 	/* sht_mouse */
 	sht_mouse = sheet_alloc(shtctl);
@@ -122,14 +111,13 @@ void KaliMain(void){
 	sheet_slide(sht_back,  0,  0);
 	sheet_slide(sht_cons[1], 56,  6);
 	sheet_slide(sht_cons[0],  8,  2);
-	sheet_slide(sht_win,  64, 56);
 	sheet_slide(sht_mouse, mx, my);
 	sheet_updown(sht_back,     0);
 	sheet_updown(sht_cons[1],  1);
 	sheet_updown(sht_cons[0],  2);
-	sheet_updown(sht_win,      3);
-	sheet_updown(sht_mouse,    4);
-	key_win = sht_win;
+	sheet_updown(sht_mouse,    3);
+	key_win = sht_cons[0];
+	keywin_on(key_win);
 	
 	/* 为了避免和键盘当前状态存在冲突，在一开始先进行设置 */
 	fifo32_put(&keycmd, KEYCMD_LED);
@@ -151,7 +139,7 @@ void KaliMain(void){
 			io_sti();
 			if (key_win->flags == 0) {	/* 输入窗口被关闭 */
 				key_win = shtctl->sheets[shtctl->top - 1];
-				cursor_c = keywin_on(key_win, sht_win, cursor_c);
+				keywin_on(key_win);
 			}
 			if (256 <= i && i <= 511) { /* 键盘数据 */
 				if (i < 0x80 + 256) { /* 将按键编码转换为字符编码 */
@@ -169,42 +157,17 @@ void KaliMain(void){
 						s[0] += 0x20;	/* 将大写字母转换为小写字母 */
 					}
 				}
-				if (s[0] != 0) { /* 一般字符 */
-					if (key_win == sht_win) {	/* 发送至任务A */
-						if (cursor_x < 128) {
-							/* 显示一个字符并将光标后移一位 */
-							s[1] = 0;
-							putfonts8_asc_sht(sht_win, cursor_x, 28, COL_BLACK, COL_WHITE, s, 1);
-							cursor_x += 8;
-						}
-					} else {	/* 发送至命令行窗口 */
-						fifo32_put(&key_win->task->fifo, s[0] + 256);
-					}
-				}
-				if (i == 256 + 0x0e) {	/* BackSpace */
-					if (key_win == sht_win) {	/* 发送至任务A */
-						if (cursor_x > 8) {
-							/* 用空格键擦除光标后将光标向前移一位 */
-							putfonts8_asc_sht(sht_win, cursor_x, 28, COL_BLACK, COL_WHITE, " ", 1);
-							cursor_x -= 8;
-						}
-					} else {	/* 发送至命令行窗口 */
-						fifo32_put(&key_win->task->fifo, 8 + 256);
-					}
-				}
-				if (i == 256 + 0x1c) {	/* Enter */
-					if (key_win != sht_win) {	/* 发送至命令行窗口 */
-						fifo32_put(&key_win->task->fifo, 10 + 256);
-					}
+				if (s[0] != 0) { /* 一般字符、BackSpace、Enter */
+					fifo32_put(&key_win->task->fifo, s[0] + 256);
 				}
 				if (i == 256 + 0x0f) {	/* Tab */
-					cursor_c = keywin_off(key_win, sht_win, cursor_c, cursor_x);
+					keywin_off(key_win);
 					j = key_win->height - 1;
 					if (j == 0) {
 						j = shtctl->top - 1;
 					}
 					key_win = shtctl->sheets[j];
-					cursor_c = keywin_on(key_win, sht_win, cursor_c);
+					keywin_on(key_win);
 				}
 				if (i == 256 + 0x2a) {	/* 左SHIFT ON */
 					key_shift |= 1;
@@ -253,11 +216,6 @@ void KaliMain(void){
 					wait_KBC_sendready();
 					io_out8(PORT_KEYDAT, keycmd_wait);
 				}
-				/* 光标再次显示 */
-				if (cursor_c >= 0) {
-					boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
-				}
-				sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
 			} else if (512 <= i && i <= 767) { /* 鼠标数据 */
 				if (mouse_decode(&mdec, i - 512) != 0) {
 					/* 鼠标指针移动 */
@@ -292,9 +250,9 @@ void KaliMain(void){
 									if (sht->buf[y * sht->bxsize + x] != sht->col_inv) {
 										sheet_updown(sht, shtctl->top - 1);
 										if (sht != key_win) {
-											cursor_c = keywin_off(key_win, sht_win, cursor_c, cursor_x);
+											keywin_off(key_win);
 											key_win = sht;
-											cursor_c = keywin_on(key_win, sht_win, cursor_c);
+											keywin_on(key_win);
 										}
 										if (3 <= x && x < sht->bxsize - 3 && 3 <= y && y < 21) {
 											mmx = mx;	/* 进入窗口移动模式 */
@@ -328,51 +286,25 @@ void KaliMain(void){
 						mmx = -1;	/* 返回通常模式 */
 					}
 				}
-			} else if (i <= 1) { /* 光标定时器 */
-				if (i != 0) {
-					timer_init(timer, &fifo, 0); /* 置0 */
-					if (cursor_c >= 0) {
-						cursor_c = COL_BLACK;
-					}
-				} else {
-					timer_init(timer, &fifo, 1); /* 置1 */
-					if (cursor_c >= 0) {
-						cursor_c = COL_WHITE;
-					}
-				}
-				timer_settime(timer, 50);
-				if (cursor_c >= 0) {
-					boxfill8(sht_win->buf, sht_win->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43);
-					sheet_refresh(sht_win, cursor_x, 28, cursor_x + 8, 44);
-				}
 			}
 		}
 	}
 }
 
-int keywin_off(struct SHEET *key_win, struct SHEET *sht_win, int cur_c, int cur_x)
+void keywin_off(struct SHEET *key_win)
 {
 	change_wtitle8(key_win, 0);
-	if (key_win == sht_win) {
-		cur_c = -1; /* 删除光标 */
-		boxfill8(sht_win->buf, sht_win->bxsize, COL_WHITE, cur_x, 28, cur_x + 7, 43);
-	} else {
-		if ((key_win->flags & 0x20) != 0) {
-			fifo32_put(&key_win->task->fifo, 3); /* 命令行窗口OFF */
-		}
+	if ((key_win->flags & 0x20) != 0) {
+		fifo32_put(&key_win->task->fifo, 3); /* 命令行窗口光标OFF */
 	}
-	return cur_c;
+	return;
 }
 
-int keywin_on(struct SHEET *key_win, struct SHEET *sht_win, int cur_c)
+void keywin_on(struct SHEET *key_win)
 {
 	change_wtitle8(key_win, 1);
-	if (key_win == sht_win) {
-		cur_c = COL_BLACK; /* 显示光标 */
-	} else {
-		if ((key_win->flags & 0x20) != 0) {
-			fifo32_put(&key_win->task->fifo, 2); /* 命令行窗口光标ON */
-		}
+	if ((key_win->flags & 0x20) != 0) {
+		fifo32_put(&key_win->task->fifo, 2); /* 命令行窗口光标ON */
 	}
-	return cur_c;
+	return;
 }
